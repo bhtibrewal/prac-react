0e92d4df7b9848704860560706399c257ce1c79c
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-12 17:00:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -12,23 +12,36 @@ function App() {
   const [mode, setMode] = useState('light');
 
   function changeMode() {
-    if(mode === 'light'){
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor="#111";
+      document.body.style.backgroundColor = "#111";
     }
-     else {
+    else {
       setMode('light');
-      document.body.style.backgroundColor="#ffffff";
-     }
+      document.body.style.backgroundColor = "#ffffff";
+    }
   }
 
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} toggleHandler={changeMode} />
-    <About mode={mode}/>
-    {/* <div className="container  my-3">
-      <Textform mode={mode} heading="Enter the text to analyze"/>
-    </div> */}
+      <Navbar title="TextUtils" mode={mode} toggleHandler={changeMode} />
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <div className="container  my-3">
+              <Textform mode={mode} heading="Enter the text to analyze" />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+
+
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,10 @@ export default function Navbar(props) {
 
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">About</a>
+              <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
 
